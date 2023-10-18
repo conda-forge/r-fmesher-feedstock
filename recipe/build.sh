6524691f8e19330d63f39e8ed5fba1102aa6f1e0
Mervin Fansler <fanslerm@mskcc.org>
2023-10-18 14:55:26 -0400

    inject flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # tolerate older osx sdk
-export PKG_CPPFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY ${PKG_CPPFLAGS}"
+sed -ie 's/PKG_CPPFLAGS=/PKG_CPPFLAGS=-D_LIBCPP_DISABLE_AVAILABILITY /g' src/Makevars
 
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
